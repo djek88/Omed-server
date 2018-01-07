483f6407b9572c9b9da171414500016734754cc9
Yevhen Zarubynskyi <zarybinskiu@gmail.com>
2018-01-07 15:59:53 +0200

    Add sdk buider for angular client application
    Change med-user model required fields
    Add category to event and listing models

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Event) {
-
+  Event.validatesInclusionOf('category', {in: ['Cours/Atelier', 'Conférence/Congrès/Séminaire', 'Concours', 'Forum/Rencontre', 'Journées scientifiques', 'Salon', 'Table ronde', 'Caravane médicale', 'Autre']}); // eslint-disable-line max-len
 };
--- common/models/listing.js
+++ common/models/listing.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(Listing) {
-
+  Listing.validatesInclusionOf('category', {in: ['Garde/Permutation', 'Remplacement', 'Offre d’emploi', 'Cession/Location de cabinet', 'Colocation', 'Vente de matériel', 'Formation', 'Dons/Bénévolat', 'Autre']}); // eslint-disable-line max-len
 };
