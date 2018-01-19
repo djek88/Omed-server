c43c9e3c41b6095179d51c374b5385e87efac4e1
Yevhen Zarubynskyi <zarybinskiu@gmail.com>
2018-01-19 11:46:47 +0200

    Enable authentication, change permissions
    Add ACLS for Account model;
    Add new degrees options, "sex" property to MedUser model;
    Add "name" validators for University, Specialty, Hospital, City, Company models;
    Rename datasources;

--- common/models/med-user.js
+++ common/models/med-user.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = function(Meduser) {
-  Meduser.validatesInclusionOf('area', {in: ['medicine', 'dental']});
+  Meduser.validatesInclusionOf('sex', {in: ['male', 'female']});
+  Meduser.validatesInclusionOf('area', {in: ['medical', 'dental']});
   Meduser.validatesInclusionOf('type', {in: ['student', 'doctor']});
-  Meduser.validatesInclusionOf('degree', {in: ['1', '2', '3', '4', '5', '6', '7', '8', 'intern1', 'intern2', 'resident', 'generalist', 'regular', 'assistant', 'agrege', 'es']}); // eslint-disable-line max-len
+  Meduser.validatesInclusionOf('degree', {in: ['1', '2', '3', '4', '5', '6', '7', '8', 'intern1', 'intern2', 'resident1', 'resident2', 'resident3', 'resident4', 'resident5', 'generalist', 'regular', 'assistant', 'agrege', 'es']}); // eslint-disable-line max-len
   Meduser.validatesInclusionOf('maritalStatus', {in: ['single', 'engaged', 'married']}); // eslint-disable-line max-len
 };
