58dec95b672d7322b656155ecb2c16cae6c8d880
Yevhen Zarubynskyi <zarybinskiu@gmail.com>
2018-01-29 20:03:39 +0200

    Add reset password with email feature
    Minor changes on datasources names;

--- common/models/account.js
+++ common/models/account.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Account) {
+const config = require('../../server/config.json');
 
+module.exports = function(Account) {
+  Account.on('resetPasswordRequest', function(info) {
+    const url = `http://${config.client.domain}/login/change-password/${info.accessToken.id}`;
+    const html = `Click <a href="${url}">here</a> to reset your password`;
+
+    Account.app.models.Email.send({
+      to: info.email,
+      from: info.email,
+      subject: 'Password reset',
+      html: html,
+    }, function(err) {
+      if (err) return console.log('> error sending password reset email');
+    });
+  });
 };
